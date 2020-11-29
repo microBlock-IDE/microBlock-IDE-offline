e668d26148a6c014de8d8e37468aab3ea15e0ab1
Max <sonthaya.nt@hotmail.com>
2020-11-29 23:34:31 +0700

    Future: Add show dashboard window

--- main.js
+++ main.js
@@ -1,29 +1,61 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path');
+var ipcMain = require('electron').ipcMain;
 
 app.allowRendererProcessReuse = false;
 
-global.sharedObj = { argv: process.argv };
+global.sharedObj = {
+    argv: process.argv,
+    dashboardWin: null
+};
 
-function createWindow () {
-  // Create the browser window.
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    },
-    icon: path.join(__dirname, "microBlock-IDE/favicon.png")
-  })
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        icon: path.join(__dirname, "microBlock-IDE/favicon.png")
+    })
+    win.loadFile("microBlock-IDE/index.html");
+    win.maximize();
 
-  // and load the index.html of the app.
-  win.loadFile('microBlock-IDE/index.html')
+    // Open the DevTools.
+    // win.webContents.openDevTools()
 
-  win.maximize()
-
-  // Open the DevTools.
-  // win.webContents.openDevTools()
+    win.on('close', () => {
+        if (global.sharedObj.dashboardWin) {
+            global.sharedObj.dashboardWin.close();
+        }
+    });
 }
+
+ipcMain.on("show-dashboard", (event) => {
+    if (global.sharedObj.dashboardWin) {
+        if (!global.sharedObj.dashboardWin.isDestroyed()) {
+            global.sharedObj.dashboardWin.focus();
+            return;
+        }
+    }
+
+    global.sharedObj.dashboardWin = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        icon: path.join(__dirname, "microBlock-IDE/favicon.png")
+    });
+
+    global.sharedObj.dashboardWin.loadFile("microBlock-IDE/dashboard/index.html");
+
+    // global.sharedObj.dashboardWin.maximize();
+
+    global.sharedObj.dashboardWin.on('close', () => {
+        global.sharedObj.dashboardWin = null;
+    });
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -34,17 +66,17 @@ app.whenReady().then(createWindow)
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
 })
 
 // In this file you can include the rest of your app's specific main process
